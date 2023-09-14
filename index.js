bd8727701e68c5f04e77f3512470edb34ff0d967
Nikhil-U-Kundar <nikhilkulalk@gmail.comgit branch -M main>
2023-09-14 22:35:49 +0530

    To_Do_List2_db

--- index.js
+++ index.js
@@ -1,65 +1,56 @@
-const express =require("express")
-const mongoose =require("mongoose")
-//onst https =require("https")
-const body =require("body-parser")
+
+const express=require("express")
+const mongoose=require("mongoose")
+const body=require("body-parser")
 const app=express()
 app.set('view engine', 'ejs');
 app.use(body.urlencoded({extended:true}))
- app.use(express.static("public"))//direct go and search the publc folder
-mongoose.connect(" mongodb+srv:mongodb+srv://nikhilukundar:9945994323@cluster0.bzxf1yq.mongodb.net/tododb ",{useNewUrlParser:true})
-// to create schema
+app.use(express.static("public"))
+
+mongoose.connect("mongodb+srv://nikhilukundar:9945994323@cluster0.bzxf1yq.mongodb.net/tododb ",{useNewUrlParser:true})
+
 const todoschema=new mongoose.Schema({task:String})
-////model is created
 const todomodel=mongoose.model("tasks",todoschema)
-//
-//const t1=new todomodel({task:"Gaming"})
-//
-//const t2=new todomodel({task:"Studying"})
-//
-//const t3=new todomodel({task:"Playing"})
-//t1.save()
+//const t1=new todomodel({task:"gaming"})
+//const t2=new todomodel({task:"studying"})
+//const t3=new todomodel({task:"playing"})
 //t2.save()
 //t3.save()
-
+//t1.save()
+var lists=[]
 
 app.get("/",function(req,res){
-
     todomodel.find().then((result) => {
         res.render('index',{tasks:result})
-    }).catch((err) => {
-       console.log(err)
-    });
     
-  
+    }).catch((err) => {
+        console.log(err)
+    });
+
 })
 
-app.post("/",function(req,res){
-var todotask= req.body.task
+app.post("/", function(req,res){
+var todotask=req.body.task
 //console.log(task)
-//lists.push(task)
-
-
+//list.push(task)
 const task=new todomodel({task:todotask})
+
 task.save()
+
 res.redirect("/")
 })
 
 app.post("/delete",function(req,res){
     var item=req.body.checkbox
-    //console.log(item)
     todomodel.deleteOne({_id:item}).then((result) => {
         res.redirect("/")
+        
     }).catch((err) => {
         console.log(err)
+        
     });
-    
 })
 
-
-
-
-
-//app.listen(3000,function(){
-    app.listen(process.env.PORT ||3000,function(){
-    console.log("server is up and running")
+app.listen(process.env.PORT ||3000,function(){
+    console.log("sever is up and running")
 })
